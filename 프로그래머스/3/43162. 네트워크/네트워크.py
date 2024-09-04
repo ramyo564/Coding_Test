7f39625fbc8b7544c4a0676272f24d184bdbbcc5
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-09-04 23:06:12 +0900

    [level 3] Title: 네트워크, Time: 0.62 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,30 @@
+from collections import deque
+def solution(n, computers):
+    visited = [[False] * n]
+    com_dic = {}
+    answer = 0
+    num = 0
+    for i in range(n):
+        for j in computers[i]:
+            if i not in com_dic:
+                com_dic[i] = []
+
+            if j == 1:
+                com_dic[i].append(num)
+            num += 1
+        num = 0
+    def bfs(com_dic, start_v):
+        queue = deque([start_v])
+        while queue:
+            cur_v = queue.popleft()
+            for v in com_dic[cur_v]:
+                if not visited[0][v]:
+                    visited[0][v] = True
+                    queue.append(v)
+
+    for i in range(n):
+        if not visited[0][i]:
+            bfs(com_dic, i)
+            answer += 1
+
+    return answer
