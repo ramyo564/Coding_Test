31ab83f0c525a2eb00c5b1386b0dd7ea9c20e66e
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-09-08 18:32:16 +0900

    [level 2] Title: 삼각 달팽이, Time: 166.93 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,21 +1,25 @@
 def solution(n):
-    answer = [[0] * n for _ in range(n)]
-    y, x = -1, 0
+    answer = []
+    triangle = [[0] * n for _ in range(n)]
+    y = -1
+    x = 0
     num = 1
-    list = []
     for i in range(n):
         for j in range(i, n):
             if i % 3 == 0:
                 y += 1
+                triangle[y][x] = num
             elif i % 3 == 1:
                 x += 1
-            elif i % 3 == 2:
+                triangle[y][x] = num
+            else:
                 y -= 1
                 x -= 1
-            answer[y][x] = num
-            num += 1
-    for i in range(n):
-        for j in range(i + 1):
-            list.append(answer[i][j])
+                triangle[y][x] = num
 
-    return list
+            num += 1
+
+    for i in range(n):
+        for j in range(i+1):
+            answer.append(triangle[i][j])
+    return answer
