53fbb61614640c5c1ff158ce4c70cd75300705f3
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-09-08 16:26:05 +0900

    [level 3] Title: 네트워크, Time: 0.66 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,30 +1,28 @@
 from collections import deque
-
 def solution(n, computers):
-    visited = [[False] * n]
-    com_dic = {}
+    visited = [False] * n
+    list = [[] * n for _ in range(n)]
     answer = 0
-    num = 0
-    for i in range(n):
-        for j in computers[i]:
-            if i not in com_dic:
-                com_dic[i] = []
-            if j == 1:
-                com_dic[i].append(num)
-            num += 1
-        num = 0
-    def bfs(com_dic, start_v):
-        queue = deque([start_v])
-        while queue:
-            cur_v = queue.popleft()
-            for v in com_dic[cur_v]:
-                if not visited[0][v]:
-                    visited[0][v] = True
-                    queue.append(v)
 
     for i in range(n):
-        if not visited[0][i]:
-            bfs(com_dic, i)
+        for j in range(n):
+            if computers[i][j] == 1:
+                list[i].append(j)
+    print(list)
+    def bfs(start):
+        queue = deque()
+        queue.append(list[i])
+        visited[i] = True
+        while queue:
+            linked_computer = queue.popleft()
+            for com in linked_computer:
+                if not visited[com] and com in list[com]:
+                    visited[com] = True
+                    queue.append(list[com])
+
+    for i in range(n):
+        if not visited[i]:
+            bfs(i)
             answer += 1
 
     return answer
