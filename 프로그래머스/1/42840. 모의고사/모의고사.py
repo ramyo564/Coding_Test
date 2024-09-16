fc7abf62abacf00985afe15a95112e74e9172956
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-09-16 12:40:23 +0900

    [level 1] Title: 모의고사, Time: 4.33 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -2,14 +2,21 @@ def solution(answers):
     num1 = [1, 2, 3, 4, 5]
     num2 = [2, 1, 2, 3, 2, 4, 2, 5]
     num3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    cnt = [0] * 3
+    quiz_len = len(answers)
 
-    for idx in range(len(answers)):
-        if answers[idx] == num1[idx % len(num1)]:
-            cnt[0] += 1
-        if answers[idx] == num2[idx % len(num2)]:
-            cnt[1] += 1
-        if answers[idx] == num3[idx % len(num3)]:
-            cnt[2] += 1
+    cnt_1 = 0
+    cnt_2 = 0
+    cnt_3 = 0
 
-    return [i + 1 for i, x in enumerate(cnt) if x == max(cnt)]
+    for idx in range(quiz_len):
+        if answers[idx] == num1[idx % 5]:
+            cnt_1 += 1
+        if answers[idx] == num2[idx % 8]:
+            cnt_2 += 1
+        if answers[idx] == num3[idx % 10]:
+            cnt_3 += 1
+
+    max_v = max(cnt_1, cnt_2, cnt_3)
+    temp = [cnt_1, cnt_2, cnt_3]
+
+    return [i + 1 for i in range(len(temp)) if temp[i] == max_v]
