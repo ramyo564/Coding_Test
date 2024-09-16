3fb7e607895dd0e98b36f53e2ba521241a99c8b4
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-09-16 16:01:39 +0900

    [level 2] Title: 카펫, Time: 0.19 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,13 +1,11 @@
 def solution(brown, yellow):
     answer = []
     total = brown + yellow
-    for i in range(1, int(total**0.5)):
-        if yellow % i == 0:
-            x = yellow // i
-            y = i
-            if (2*x) + (2*y) + 4 == brown:
-                answer.append(x+2)
-                answer.append(y+2)
-                break;
-    answer.sort(reverse=True)
-    return answer
+
+    for y in range(1, total):
+        if yellow % y == 0:
+            x = yellow // y
+        if (2 * x) + (2 * y) + 4 == brown:
+            answer.append(x + 2)
+            answer.append(y + 2)
+            return answer
