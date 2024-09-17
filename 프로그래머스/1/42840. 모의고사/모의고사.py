a372fdafff9be266a6a4e5e5a6d4e69c6b6e3972
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-09-17 12:55:53 +0900

    [level 1] Title: 모의고사, Time: 2.22 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,15 +1,15 @@
 def solution(answers):
-    num1 = [1, 2, 3, 4, 5]
-    num2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    num3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    cnt = [0] * 3
+    student1 = [1, 2, 3, 4, 5]
+    student2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    answer = [0, 0, 0]
 
-    for idx in range(len(answers)):
-        if answers[idx] == num1[idx % len(num1)]:
-            cnt[0] += 1
-        if answers[idx] == num2[idx % len(num2)]:
-            cnt[1] += 1
-        if answers[idx] == num3[idx % len(num3)]:
-            cnt[2] += 1
+    for idx, v in enumerate(answers):
 
-    return [i + 1 for i, x in enumerate(cnt) if x == max(cnt)]
+        if v == student1[idx % 5]:
+            answer[0] += 1
+        if v == student2[idx % 8]:
+            answer[1] += 1
+        if v == student3[idx % 10]:
+            answer[2] += 1
+    return [idx + 1 for idx, x in enumerate(answer) if x == max(answer)]
