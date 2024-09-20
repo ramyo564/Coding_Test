eab0ce82cd165df0b0b9fdbe642434f2225f8e6a
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-09-20 10:19:36 +0900

    Time: 10 ms (80.89%), Space: 11.6 MB (34.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,3 @@
-dic = {}
-
-
 class Solution(object):
     def climbStairs(self, n):
         """
@@ -9,10 +6,12 @@ class Solution(object):
         """
         if n == 1:
             return 1
-        if n == 2:
-            return 2
 
-        if n not in dic:
-            dic[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
 
-        return dic[n]
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
