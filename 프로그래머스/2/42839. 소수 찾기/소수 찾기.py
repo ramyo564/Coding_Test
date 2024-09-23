b4ec19e6e1ae00761fcdbca00c1da3e7b56f08c3
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-09-23 17:49:59 +0900

    [level 2] Title: 소수 찾기, Time: 20.91 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,22 +1,23 @@
 from itertools import permutations
 
 
-def check(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
+def solution(numbers):
+    set_list = set()
+
+    def check(num):
+        if num == 2:
+            return True
+        if num < 2:
             return False
 
-    return True
+        for i in range(2, int(num ** 0.5)+1):
+            if num % i == 0:
+                return False
+        return True
 
+    for i in range(len(numbers)):
+        for p in permutations(numbers, i + 1):
+            if check(int(''.join(p))):
+                set_list.add(int(''.join(p)))
 
-def solution(numbers):
-    answer = 0
-    j = set()
-    for idx, _ in enumerate(numbers):
-        for i in permutations(numbers, idx + 1):
-            x = int(''.join(i))
-            if check(x):
-                j.add(x)
-    return len(j)
+    return len(set_list)
