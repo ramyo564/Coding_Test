15d6a5964b20a30bdf38bfc1e2af229f6326643d
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-11-18 23:45:35 +0900

    [level 2] Title: 전화번호 목록, Time: 445.12 ms, Memory: 46.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,13 @@
 def solution(phone_book):
+    dic = {}
+    for num in phone_book:
+        dic[num] = 1
 
-    # phone_book 정렬 -> 숫자로 된 문자열의 정렬은 사전식을 따른다.
-    # ["1", "2", "3", "10"] -> ["1", "10", "2", "3"]
-    phone_book.sort()
+    for nums in phone_book:
+        arr = ""
+        for num in nums:
+            arr += num
 
-    # 반복문을 수행하면서 현재 번호와 다음 번호만 접두사 비교를 해줌 -> 하나라도 겹치면 False를 반환하기 때문에 가능
-    for i in range(len(phone_book)-1):
-        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
-            return False
-
+            if arr in dic and arr != nums:
+                return False
     return True
