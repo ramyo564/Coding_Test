0b8ff596c1dd0dfd9a3ef9225a213aeaf157394d
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-11-23 21:28:45 +0900

    [level 2] Title: 가장 큰 수, Time: 65.23 ms, Memory: 27.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,12 +1,11 @@
 def solution(numbers):
-    # 숫자를 문자열로 변환
-    temp = list(map(str, numbers))
-    # 정렬: 두 숫자를 이어붙였을 때 큰 순서로 정렬
+    temp = []
+    for i in numbers:
+        temp.append(str(i))
     temp.sort(key=lambda x: x*3, reverse=True)
-    # 정렬 결과를 이어붙여 최종 답안 생성
-    answer = ''.join(temp)
-    # 모든 숫자가 0인 경우를 처리
-    return answer if answer[0] != '0' else '0'
 
-numbers = [3, 30, 34, 5, 9]
-print(solution(numbers))  # "9534330"
+    answer = ''
+    for i in temp:
+        answer += i
+        
+    return answer if answer[0] != '0' else '0'
