84fd4a6cb133630be839b79b2aeb7aedc7631fcf
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-12-06 12:44:29 +0900

    [level 1] Title: 모의고사, Time: 4.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,15 +1,22 @@
 def solution(answers):
-    num1 = [1, 2, 3, 4, 5]
-    num2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    num3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    cnt = [0, 0, 0]
+    people_1 = [1, 2, 3, 4, 5]
+    people_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    people_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
-    for idx, v in enumerate(answers):
-        if answers[idx] == num1[idx % len(num1)]:
-            cnt[0] += 1
-        if answers[idx] == num2[idx % len(num2)]:
-            cnt[1] += 1
-        if answers[idx] == num3[idx % len(num3)]:
-            cnt[2] += 1
+    temp = [0, 0, 0]
+    for i in range(len(answers)):
 
-    return [idx+1 for idx, v in enumerate(cnt) if max(cnt) == v]
+        if people_1[i % len(people_1)] == answers[i]:
+            temp[0] += 1
+        if people_2[i % len(people_2)] == answers[i]:
+            temp[1] += 1
+        if people_3[i % len(people_3)] == answers[i]:
+            temp[2] += 1
+
+    target = max(temp)
+    answer = []
+    for i in range(len(temp)):
+        if temp[i] == target:
+            answer.append(i+1)
+
+    return answer
