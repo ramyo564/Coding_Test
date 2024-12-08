68ee0f33b6d4ab95eb514f039d563285f08f5015
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-12-09 00:14:39 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.40 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,9 +1,11 @@
-from collections import deque
+
 from itertools import combinations
+from collections import deque
 
 
 def solution(n, wires):
     answer = n
+
     def bfs(start):
         visited[start] = True
         queue = deque([start])
@@ -12,20 +14,20 @@ def solution(n, wires):
             cur_v = queue.popleft()
             for v in graph[cur_v]:
                 if not visited[v]:
-                    queue.append(v)
                     visited[v] = True
+                    queue.append(v)
                     cnt += 1
         return cnt
 
     for c in combinations(wires, len(wires) - 1):
         graph = [[] for _ in range(n + 1)]
         visited = [False] * (n + 1)
-        for i in c:
-            graph[i[0]].append(i[1])
-            graph[i[1]].append(i[0])
+        for first_node, second_node in c:
+            graph[first_node].append(second_node)
+            graph[second_node].append(first_node)
 
-        first = bfs(1)
-        second = n - first
-        answer = min(abs(first-second), answer)
+        first_network = bfs(1)
+        second_network = n - first_network
+        answer = min(answer, abs(first_network - second_network))
 
     return answer
