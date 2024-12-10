cac0ab117da60ab1b4b529755df8cf1be88e9cef
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-12-10 12:08:38 +0900

    [level 3] Title: 네트워크, Time: 0.56 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,32 +1,30 @@
-
 from collections import deque
 
 
 def solution(n, computers):
-    visited = [False] * n
-    graph = [[] * n for _ in range(len(computers))]
-    answer = 0
-
-    for i in range(len(computers)):
-        for j in range(len(computers[0])):
-            if computers[i][j] == 1:
-                graph[i].append(j)
-
     def bfs(start):
         visited[start] = True
-        queue = deque()
-        queue.append(start)
-
+        queue = deque([start])
         while queue:
             cur_v = queue.popleft()
-            for v in graph[cur_v]:
-                if not visited[v]:
-                    visited[v] = True
-                    queue.append(v)
+            for i in graph[cur_v]:
+                if not visited[i]:
+                    visited[i] = True
+                    queue.append(i)
+        return 1
+
+    graph = {}
+    visited = [False] * n
+    answer = 0
+    for idx, value in enumerate(computers):
+        for value_index, v in enumerate(value):
+            if idx not in graph and v == 1:
+                graph[idx] = [value_index]
+            elif v == 1:
+                graph[idx].append(value_index)
 
     for i in range(n):
         if not visited[i]:
-            bfs(i)
-            answer += 1
+            answer += bfs(i)
 
     return answer
