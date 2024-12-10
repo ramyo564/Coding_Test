7ed85d0b1a970b7abf9e9af141a7d5672c6153d9
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-12-10 15:51:14 +0900

    [level 2] Title: 타겟 넘버, Time: 306.83 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,19 +2,23 @@ from collections import deque
 
 
 def solution(numbers, target):
-    answer = 0
     queue = deque()
-    queue.append([numbers[0] * -1, 0])
-    queue.append([numbers[0], 0])
+    queue.append(numbers[0])
+    queue.append(numbers[0] * -1)
+    queue.append("END")
+    cnt = 0
 
-    while queue:
-        cur_v, idx = queue.popleft()
-        idx += 1
-        if idx < len(numbers):
-            queue.append([cur_v + numbers[idx], idx])
-            queue.append([cur_v - numbers[idx], idx])
-        else:
-            if cur_v == target:
-                answer += 1
+    for i in range(1, len(numbers)):
+        while queue[0] != "END":
+            temp = queue.popleft()
+            queue.append(temp + numbers[i])
+            queue.append(temp - numbers[i])
 
-    return answer
+        queue.popleft()
+        queue.append("END")
+
+    for x in queue:
+        if x == target:
+            cnt += 1
+
+    return cnt 
