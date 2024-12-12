56b0ab9533d8687fca6bcd175435f92441d66ed6
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-12-12 20:51:26 +0900

    [level 3] Title: 단어 변환, Time: 0.80 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,27 +1,26 @@
 from collections import deque
 
+
 def solution(begin, target, words):
-    answer = 0
-    q = deque()
-    q.append([begin, 0])    # [단어, 깊이]
-    V = [ 0 ] * (len(words))    # 방문 노드 여부 확인 리스트
-    while q:
-        word, cnt = q.popleft()
-        if word == target:
-            answer = cnt
-            break
-        for i in range(len(words)):
-            temp_cnt = 0
-            if not V[i]:    # 만약 확인 안 한 단어라면
-                # 그 단어가 words 속 단어와 다를 때 한 자씩 비교해서 더하기
-                for j in range(len(word)):
-                    if word[j] != words[i][j]:
-                        temp_cnt += 1
+    if target not in words:
+        return 0
+    n = len(words)
+    visited = [False] * n
+    queue = deque([[begin, 0]])
+    while queue:
+        temp, cnt = queue.popleft()
+        if temp == target:
+            return cnt
 
-                if temp_cnt == 1:   # 만약 다른 글자 개수가 1개라면
-                    q.append([words[i], cnt+1])
-                    V[i] = 1
+        for i in range(n):
+            if not visited[i]:
+                difference = 0
 
-    return answer
+                for r in range(len(words[i])):
+                    if temp[r] != words[i][r]:
+                        difference += 1
 
-
+                if difference == 1:
+                    next = words[i]
+                    visited[i] = True
+                    queue.append([next, cnt + 1])
