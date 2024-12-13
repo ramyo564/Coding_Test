4d3b4e077294443988226579cd2106e2d04c2a3b
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-12-13 19:43:05 +0900

    [level 3] Title: 여행경로, Time: 687.31 ms, Memory: 27.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,19 +1,23 @@
+
 from collections import deque
+
+
 def solution(tickets):
     answer = []
     q = deque()
-    q.append(("ICN",["ICN"], []))
-    
+    q.append((["ICN"], []))
+
     while q:
-        airport, path, used = q.popleft()
+        path, used = q.popleft()
 
         if len(used) == len(tickets):
             answer.append(path)
-        
+
         for idx, ticket in enumerate(tickets):
-            if ticket[0] == airport and not idx in used:
-                q.append((ticket[1], path+[ticket[1]], used+[idx]))
-                
-    
+            depart, arrived = ticket
+            if depart == path[-1] and not idx in used:
+                q.append((path +[arrived], used + [idx]))
+
     answer.sort()
+
     return answer[0]
