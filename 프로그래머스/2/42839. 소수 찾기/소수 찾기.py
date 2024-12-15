98182ae4c48b3c1af1d32202f0bc66cb97618cdf
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-12-16 01:21:38 +0900

    [level 2] Title: 소수 찾기, Time: 5.92 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,23 +1,20 @@
 from itertools import permutations
-
+from math import sqrt
 
 def solution(numbers):
-    set_list = set()
+    temp_set = set()
+    answer = set()
+    for i in range(1, len(numbers) + 1):
+        for temp in permutations(numbers, i):
+            temp_set.add(int("".join(temp)))
 
-    def check(num):
-        if num == 2:
-            return True
-        if num < 2:
-            return False
-
-        for i in range(2, int(num ** 0.5)+1):
-            if num % i == 0:
-                return False
-        return True
-
-    for i in range(len(numbers)):
-        for p in permutations(numbers, i + 1):
-            if check(int(''.join(p))):
-                set_list.add(int(''.join(p)))
-
-    return len(set_list)
+    for i in temp_set:
+        if i > 1:  # 0과 1은 소수가 아님
+            is_prime = True
+            for j in range(2, int(sqrt(i)) + 1):
+                if i % j == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                answer.add(i)
+    return len(answer)
