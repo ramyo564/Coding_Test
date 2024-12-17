ab178ec194d20c2ab4aa4d5d43416947406c09ba
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-12-17 19:22:26 +0900

    [level 3] Title: 아이템 줍기, Time: 1.77 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,37 +1,50 @@
 from collections import deque
 
+
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    answer = 0
-    graph = [[-1 for _ in range(102)] for _ in range(102)]
-    visited = [[1 for _ in range(102)] for _ in range(102)]
-    direction = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-    dq = deque()
-    
-    for r in rectangle:
-        x1, y1, x2, y2 = map(lambda x: x*2, r)
-        for i in range(x1, x2+1):
-            for j in range(y1, y2+1):
-                if x1 < i < x2 and y1 < j < y2:
-                    graph[i][j] = 0
+    def bfs(y, x, item_y, item_x, visited, graph):
 
-                elif graph[i][j] != 0:
-                    graph[i][j] = 1                
+        dx = [1, -1, 0, 0]
+        dy = [0, 0, 1, -1]
 
-    cx, cy, ix, iy = 2*characterX, 2*characterY, 2*itemX, 2*itemY
-    
-    dq.append((cx, cy))
-    
-    while dq:
-        x, y = dq.popleft()
-        
-        if x == ix and y == iy:
-            answer = visited[x][y] // 2
-            break   
-        for k in range(4):
-            nx, ny = x + direction[k][0], y + direction[k][1]
-            
-            if graph[nx][ny] == 1 and visited[nx][ny] == 1:
-                visited[nx][ny] += visited[x][y]
-                dq.append((nx, ny))
+        visited[y][x] = True
+        cnt = 0
+        queue = deque()
+        queue.append([y, x, cnt])
+        while queue:
+            cur_y, cur_x, cnt = queue.popleft()
+            if cur_y == item_y and cur_x == item_x:
+                return int(cnt // 2)
 
-    return answer
+            for i in range(4):
+                next_y = cur_y + dy[i]
+                next_x = cur_x + dx[i]
+
+                if graph[next_y][next_x] == 1 and not visited[next_y][next_x]:
+                    visited[next_y][next_x] = True
+                    queue.append([next_y, next_x, cnt + 1])
+
+    def make_graph(data):
+        start_x = data[0] * 2
+        start_y = data[1] * 2
+        end_x = data[2] * 2
+        end_y = data[3] * 2
+
+        # 로직 확인하면 2배 해줘야함
+        for y in range(start_y, end_y + 1):
+            for x in range(start_x, end_x + 1):
+                if start_y < y < end_y and start_x < x < end_x:
+                    graph[y][x] = 3
+                else:
+                    if graph[y][x] == 3:
+                        continue
+                    else:
+                        graph[y][x] = 1
+
+    graph = [[0] * 102 for _ in range(102)]
+    visited = [[False] * 102 for _ in range(102)]
+
+    for data in rectangle:
+        make_graph(data)
+
+    return bfs(characterY * 2, characterX * 2, itemY * 2, itemX * 2, visited, graph)
