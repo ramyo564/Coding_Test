d31eab5ac12ffd2983a99bce3cef6c64a4d680c0
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-03-19 22:01:49 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,34 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 	
+	string name, result;
 	char letter;
 	
 int main(){
-	string name;
-	
+
 	getline(cin, name);
-	string result(name.length(), ' '); 
-	
 	for (int i = 0; i < name.length(); i++){
 		letter = name[i];
 		if (65<= letter && letter <= 90){
 			if(77 < letter){
-				result[i] = 'A' + letter - 78;
+				result += 'A' + letter - 78;
 			}
 			else{
-				result[i] = letter + 13;
+				result += letter + 13;
 			}
 		}
 		else if (97 <= letter && letter <= 122){
 			if(109 < letter){
-				result [i] = 'a' + letter - 110;
+				result += 'a' + letter - 110;
 			}
 			else {
-				result [i] = letter + 13;
+				result += letter + 13;
 			}
 		}
 		else {
-			result[i] = letter;
+			result += letter;
 		}
 	}
 	
