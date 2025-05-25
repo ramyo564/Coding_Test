05d00d0eb41793766e19838472a0c29e2745de2e
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-05-25 20:56:15 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,22 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-
+int main()
+{
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	string target;
-	cin >> target;
-	string a(target.rbegin(),target.rend());
-	
-	if (a == target){
+
+	string str; 
+	cin >> str;
+
+	string ret = str;
+
+	reverse(ret.begin(), ret.end());
+
+	if(str == ret){
 		cout << 1;
-	} else {
-		
+	}else {
 		cout << 0;
 	}
-	
-	
+
 	return 0;
 }
