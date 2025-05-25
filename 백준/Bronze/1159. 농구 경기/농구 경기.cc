7d724b1ea685bb197b96cb5c91ecef12b6ade1ff
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-05-25 21:45:01 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -1,38 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-	
-	int people, cnt[26];
-	string name, result;
-	char lastName;
-	
-int main(){
 
+int arr[26];
+
+int main()
+{
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+
 	
-	cin >> people;
-	
-	for (int i = 0; i<people; i++){
-		cin >> name;
-		lastName = name[0];
-		cnt[lastName - 'a'] ++;
+
+	int n;
+	cin >> n;
+	string str;
+
+	string ret = "";
+	for (int i = 0; i < n; i++){
+		cin >> str;
+		arr[str[0] -'a']++;
 	}
-	
-	for (int i = 0; i<26; i++){
-		
-		if(cnt[i] > 4) {
-			result += (char)(i+'a');	
+
+	for (int i =0; i<26; i++){
+		if ( 5 <= arr[i] ){
+			ret += (i+'a');	
 		}
 	}
-	
-	if (result.empty()){
-		cout << "PREDAJA" << "\n";
-	}
-	else{
-		cout << result << "\n";
+
+	if(ret.size()){
+		cout << ret;
+	}else{
+		cout << "PREDAJA";
 	}
 
-
-    return 0;
+	return 0;
 }
