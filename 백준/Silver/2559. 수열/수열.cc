7e2d15c906bcc25734ff72cffd0a0e723c5b9c8b
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-07-25 15:10:06 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,17 +1,35 @@
-#include<bits/stdc++.h> 
-using namespace std;  
-typedef long long ll;  
-int n, k, temp, psum[100001], ret = -1000000; 
-int main(){
-	ios_base::sync_with_stdio(false);
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);cout.tie(NULL);
-	cin >> n >> k; 
-	for(int i = 1; i <= n; i++){
-		cin >> temp; psum[i] = psum[i - 1] + temp; 
-	} 
-	for(int i = k; i <= n; i++){
-		ret = max(ret, psum[i] - psum[i - k]);
-	}
-	cout << ret << "\n";
+    int N, K, num, lastIdx, temp, ret=0, startIdx=0;
+    int arr [100001] = {};
+    cin >> N >> K;
+    
+    for (int i = 0; i < N; i++){
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < K; i++){
+        ret += arr[i];
+    }
+    
+    temp = ret;
+    for (int i = K; i < N; i++){
+        
+        temp = temp - arr[startIdx] + arr[i];
+        if (temp > ret){
+            ret = temp;
+        }
+        startIdx += 1;
+    }
+
+    cout << ret;
+
+
+
     return 0;
 }
