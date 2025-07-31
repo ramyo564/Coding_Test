81142a0b953a6be56e10cab0aad589a9af86adcb
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-07-31 13:47:55 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -1,32 +1,32 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-        // StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         String target = br.readLine();
-        
-        int pos = target.indexOf("*");
-        String front = target.substring(0, pos);
-        String back = target.substring(pos + 1);
+        String front, back, temp;
+        int pos;
 
-        for (int i = 0; i < N; i ++){
-            String temp = br.readLine();
-            if (temp.length() < front.length() + back.length()){
+        pos = target.indexOf("*");
+        front = target.substring(0, pos);
+        back = target.substring(pos + 1);
+
+        for (int i = 0; i < N; i++) {
+            temp = br.readLine();
+            if (temp.length() < front.length() + back.length()) {
                 System.out.println("NE");
                 continue;
             }
-            if (temp.substring(0,front.length()).equals(front) 
-            && temp.substring(temp.length()-back.length()).equals(back)){
+            if (temp.substring(0, front.length()).equals(front)
+                    && temp.substring(temp.length() - back.length()).equals(back)) {
                 System.out.println("DA");
-            }else{
+            } else {
                 System.out.println("NE");
             }
 
         }
-
     }
 }
