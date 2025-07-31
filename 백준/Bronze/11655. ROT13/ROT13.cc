01b6c41b3127b381b65d6c4883334f971ef44cff
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-07-31 11:36:58 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,38 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+int main() {
+  string str, ret;
+  getline(cin, str);
 
-int arr[26];
-string str;
-string ret;
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+  for (char s : str){
+    if (s >= 'A' && s <= 'Z'){
+      ret += (s - 'A' + 13) % 26 + 'A';
+    }
+    else if (s >= 'a' && s <= 'z')
+    {
+      ret += (s - 'a' + 13) % 26 + 'a';
+    }
+    else {
+      ret += s;
+    }
+    
+  }
+  cout << ret;
 
-	// A-Z 65 ~ 90
-	// a-z 97 ~ 122
-	getline(cin, str);
-
-	// cout << str;
-
-	for (char s : str)
-	{
-		// 대문자
-		if (s >= 'A' && s <= 'Z')
-		{
-			ret += (s-'A' + 13) %26 + 'A';
-		}
-		// 소문자
-		else if (s >= 'a' && s <= 'z')
-		{
-			ret += (s-'a' +13) % 26 +'a';
-		}
-		else
-		{
-			ret += s;
-		}
-	}
-	cout << ret;
-	return 0;
+  return 0;
 }
