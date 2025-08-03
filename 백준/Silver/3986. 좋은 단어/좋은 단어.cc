d7928503144afde89d84bc2599ec89e3aa862ef9
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-08-03 12:13:41 +0900

    [Silver IV] Title: 좋은 단어, Time: 0 ms, Memory: 2488 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -1,45 +1,33 @@
-#include <iostream>
-#include <string>
-#include <stack>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    // 입출력 속도 향상 (백준에서는 필수)
+int N;
+int ret;
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    int N;
+    
     cin >> N;
-
-    int good_word_count = 0;
-    for (int i = 0; i < N; i++) {
-        string word;
-        cin >> word;
-
-        // 단어 길이가 홀수이면 절대 좋은 단어가 될 수 없음
-        if (word.length() % 2 != 0) {
-            continue;
-        }
-
-        stack<char> s;
-        for (char c : word) {
-            // 스택이 비어있지 않고, top의 문자가 현재 문자와 같다면 pop (짝을 찾음)
-            if (!s.empty() && s.top() == c) {
-                s.pop();
-            }
-            // 그렇지 않다면 push
-            else {
-                s.push(c);
+    
+    for (int i = 0; i < N; i++)
+    {
+        stack<char> st;
+        string s;
+        cin >> s;
+        for (int j = 0; j < s.length(); j++)
+        {
+            if(st.empty() || st.top() != s[j]){
+                st.push(s[j]);
+            }else{
+                st.pop();
             }
         }
-
-        // 모든 문자를 처리한 후 스택이 비어있으면 좋은 단어
-        if (s.empty()) {
-            good_word_count++;
-        }
+        if(st.empty())
+            ret++;
+        
+        
     }
-
-    cout << good_word_count;
+    cout << ret;
     return 0;
 }
