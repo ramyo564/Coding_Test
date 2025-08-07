5f4fc40e557a545b03c71f2e0712153ce52520ef
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-08-07 16:43:47 +0900

    [Bronze I] Title: ROT13, Time: 92 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.java
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -1,26 +1,25 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String target = br.readLine();
-        String ret  = "";
-        for (int i = 0; i < target.length(); i++) {
-            char c = target.charAt(i);
+        String st = br.readLine();
+        String ret = "";
 
-            if ('A' <= c && 'Z' >= c) {
-                ret += (char) ((c - 'A' + 13) % 26 + 'A');
+        for (int i = 0; i < st.length(); i++) {
+            if (st.charAt(i) <= 'Z' && st.charAt(i) >= 'A') {
+                ret += (char) ((st.charAt(i) - 'A' + 13) % 26 + 'A');
             }
-            else if ('a' <= c && 'z' >= c) {
-                ret += (char) ((c - 'a' + 13) % 26 + 'a');
+            else if (st.charAt(i) <= 'z' && st.charAt(i) >= 'a') {
+                ret += (char) ((st.charAt(i) - 'a' + 13) % 26 + 'a');
             }
-            else {
-                ret += c;
+            else{
+                ret += st.charAt(i);
             }
         }
-
         System.out.println(ret);
+
     }
 }
