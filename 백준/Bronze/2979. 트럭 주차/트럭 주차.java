4b96bcc0e388fd5a77d993caa078cef71f8373f5
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-08-07 16:55:44 +0900

    [Bronze II] Title: 트럭 주차, Time: 100 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.java
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.java
@@ -5,38 +5,32 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A, B, C, fr, bk, ret = 0;
+        int[] arr = new int[101];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        int arr[] = new int[101];
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < 3 ; i++){
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            
-            for (int j = start + 1; j <= end; j++){
-                arr[j] ++;
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            fr = Integer.parseInt(st2.nextToken());
+            bk = Integer.parseInt(st2.nextToken());
+            for (int j = fr; j < bk; j++) {
+                arr[j]++;
             }
         }
 
-        int ret = 0;
-        for (int i = 0; i < 101; i++){
-            if (arr[i] == 3){
+        for (int i = 0; i < 101; i++) {
+            if (arr[i] == 1) {
+                ret += arr[i] * A;
+            } else if (arr[i] == 2) {
+                ret += arr[i] * B;
+            } else {
                 ret += arr[i] * C;
             }
-            else if (arr[i] == 2){
-                ret += arr[i] * B;
-            }
-            else if (arr[i] == 1){
-                ret += arr[i] * A;
-            }
         }
-
         System.out.println(ret);
-
 
     }
 }
