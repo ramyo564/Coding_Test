7c8b6e19d626bcb2d227664cbc366087cc648f5d
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-08-08 16:32:53 +0900

    [Silver III] Title: 수열, Time: 228 ms, Memory: 23704 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,35 +1,35 @@
 import java.io.*;
 import java.util.*;
- 
+
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int temp = 0, ret = 0, startIdx = 0;
+        int[] arr = new int[N];
 
-        int[] arr = new int[100001];
-
-        st = new StringTokenizer(br.readLine());
+        int ret = 0, temp = 0;
+        
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st2.nextToken());
+            arr[i] = num;
         }
 
         for (int i = 0; i < K; i++) {
             ret += arr[i];
         }
-
         temp = ret;
-        for (int i = K; i < N; i++) {
-            temp = temp - arr[startIdx] + arr[i];
-            if (temp > ret) {
+        int j = 0;
+        for ( int i = K; i < N; i++){
+            temp = temp - arr[j] + arr[i];
+            if (ret < temp){
                 ret = temp;
             }
-            startIdx += 1;
+            j++;
         }
-
         System.out.println(ret);
-
     }
 }
