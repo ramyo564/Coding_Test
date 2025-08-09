69579b7de01f05c240432aaec2da42a08475e28d
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-08-09 16:01:07 +0900

    [Silver IV] Title: 주몽, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,35 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, target;
+int N, K, ret;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	
-	cin >> N >> target;
-	vector<int> arr(N);	
-	for (int i = 0; i < N; i++){
-		cin >> arr[i];
-	}
-	
-	sort(arr.begin(), arr.end());
-	
-	int left = 0;
-	int right = arr.size()-1;
-	int ret = 0;
-	while(left != right){
-		if (arr[left] + arr[right] < target){
-			left ++;
-		}
-		else if (arr[left] + arr[right] > target){
-			right --;
-		}else{
-			left ++;
-			ret ++;
-		}
-	}
-	cout << ret << '\n';
-	
+    cin >> N >> K;
+
+    int arr[N] = {};
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+
+    int r = N - 1;
+    int l = 0;
+
+    while (r != l)
+    {
+        if (arr[r] + arr[l] == K)
+        {
+            ret++;
+            l++;
+        }
+        else if (arr[r] + arr[l] < K)
+        {
+            l++;
+        }
+        else
+        {
+            r--;
+        }
+    }
+    cout << ret;
+
     return 0;
 }
