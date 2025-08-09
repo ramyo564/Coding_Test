256cf0517e992e5794dc98b324c5ac349417c00d
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-08-09 15:44:20 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 172 ms, Memory: 28252 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,31 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M;
-unordered_map<string, string> mp;
-
+int N, K;
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin >> N >> M;
-
+    cin >> N >> K;
+    string temp;
+    unordered_map<string, string> num_ky_map;
+    unordered_map<string, int> st_ky_map;
     for (int i = 0; i < N; i++)
     {
-        string st, num;
-        cin >> st;
-        num = to_string(i + 1);
-        mp[st] = (num);
-        mp[num] = st;
+        cin >> temp;
+        num_ky_map[to_string(i + 1)] = temp;
+        st_ky_map[temp] = i + 1;
     }
-
-    for (int i = 0; i < M; i++)
-    {
-        string st;
-        cin >> st;
-
-        cout << mp[st] << "\n";
+    for (int i = 0; i < K; i++){
+        cin >> temp;
+        if(num_ky_map.count(temp)){
+            cout << num_ky_map[temp] << '\n';
+        }else {
+            cout << st_ky_map[temp] << '\n';
+        }
     }
 
     return 0;
