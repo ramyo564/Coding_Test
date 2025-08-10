d121c5a3dd07801c33f50ac2cdfa883b23ce4b82
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-08-10 14:56:22 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -5,26 +5,24 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
+        int N = Integer.parseInt(br.readLine());
+        
         for (int i = 0; i < N; i++) {
             int M = Integer.parseInt(br.readLine());
             Map<String, Integer> map = new HashMap<>();
-
             for (int j = 0; j < M; j++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                String a = st.nextToken();
-                String b = st.nextToken();
-                map.put(b, map.getOrDefault(b, 0) + 1);
+                st.nextToken();
+                String key = st.nextToken();
+                map.put(key, map.getOrDefault(key, 0) +1);
             }
-
-            int temp = 1;
+            int ret = 1;
             for (Map.Entry<String, Integer> entry : map.entrySet()){
-                temp *= (entry.getValue() + 1);
-
+                ret *= entry.getValue() + 1;
             }
 
-            System.out.println(temp - 1);
+            System.out.println(ret - 1);
         }
 
     }
