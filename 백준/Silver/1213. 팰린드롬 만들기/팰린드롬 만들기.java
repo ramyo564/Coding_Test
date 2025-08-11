f2c687bfaf123e2c5cebf073553ff32d1e694a0c
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-08-11 17:07:44 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -5,46 +5,37 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String st = br.readLine();
-        String ret = "";
+        String temp = br.readLine();
+
+        int[] arr = new int[26];
+        for (int i = 0; i < temp.length(); i++) {
+            arr[temp.charAt(i) - 'A']++;
+        }
         char center = ' ';
         int cnt = 0;
-        boolean flag = true;
-        int[] arr = new int[26];
-        for (int i = 0; i < st.length(); i++) {
-            arr[st.charAt(i) - 'A']++;
-        }
-
         for (int i = 0; i < 26; i++) {
             if (arr[i] % 2 != 0) {
                 center = (char) (i + 'A');
                 cnt++;
             }
         }
-        if (cnt > 1) {
-            flag = false;
+        String ret = "";
+        for (int i = 0; i < 26; i++) {
+
+            for (int j = 0; j < arr[i] / 2; j++) {
+                ret += (char) (i + 'A');
+            }
         }
-        StringBuilder sb = new StringBuilder(ret);
-
-        if (flag) {
-            for (int i = 0; i < 26; i++) {
-                int temp = 0;
-                temp = arr[i] / 2;
-                for (int j = 0; j < temp; j++) {
-                    sb.append((char) (i + 'A'));
-                }
-
-            }
-            String front = sb.toString();
-            String back = sb.reverse().toString();
-
-            if (center != ' ') {
-                System.out.println(front + center + back);
-            } else {
-                System.out.println(front + back);
-            }
-        } else {
+        String rev = "";
+        for (int i = ret.length() - 1; i >= 0; i--) {
+            rev += ret.charAt(i);
+        }
+        if (cnt > 1){
             System.out.println("I'm Sorry Hansoo");
+        }else if (center == ' '){
+            System.out.println(ret+rev);
+        }else{
+            System.out.println(ret+center+rev);
         }
 
     }
