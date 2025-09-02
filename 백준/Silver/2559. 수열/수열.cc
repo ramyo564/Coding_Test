146af8b3a7cc5b706a39e384cb3e3d5d35ec12e8
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-09-02 12:35:34 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, K;
+int N, M, temp, ret;
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int ret = 0, temp = 0;
-    cin >> N >> K;
+
+    cin >> N >> M;
     int arr[N] = {};
 
     for (int i = 0; i < N; i++)
@@ -15,21 +16,20 @@ int main()
         cin >> arr[i];
     }
 
-    for (int i = 0; i < K; i++)
+    for (int i = 0; i < M; i++)
     {
-        ret += arr[i];
+        temp += arr[i];
     }
 
-    temp = ret;
-    int j = 0;
-    for (int i = K; i < N; i++)
-    {
-        temp = temp - arr[j] + arr[i];
-        if (ret < temp)
-        {
+    ret = temp;
+    int idx = 0;
+    for (int i = M; i < N; i++){
+        temp -= arr[idx];
+        temp += arr[i];
+        if(temp > ret){
             ret = temp;
         }
-        j++;
+        idx++;
     }
 
     cout << ret;
