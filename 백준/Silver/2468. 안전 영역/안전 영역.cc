21595e819062fdfd5549cfaf9f7f8d12388ebc92
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2025-09-20 20:49:14 +0900

    [Silver I] Title: 안전 영역, Time: 28 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -10,16 +10,11 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int N;
-    int temp;
+
+    int N, height = 0, temp, ret = 0;
+
     cin >> N;
-
-    int height = 0;
-    int answer = 0;
-
     vector<vector<int>> arr(N, vector<int>(N, 0));
-    queue<Point> q;
-    queue<Point> q_2;
 
     for (int i = 0; i < N; i++)
     {
@@ -33,47 +28,50 @@ int main()
             }
         }
     }
-    int d_y[] = {0, 0, -1, 1};
-    int d_x[] = {-1, 1, 0, 0};
-    // N = y
-    // M = x
+
     while (height > -1)
     {
         vector<vector<bool>> visited(N, vector<bool>(N, false));
+        queue<Point> q;
         for (int i = 0; i < N; i++)
         {
             for (int r = 0; r < N; r++)
             {
-
-                if (arr[i][r] > height - 1)
+                if (arr[i][r] > height)
                 {
                     q.push({i, r});
                 }
             }
         }
-        int cnt = 0;
+
+        int d_y[4] = {-1, 1, 0, 0};
+        int d_x[4] = {0, 0, -1, 1};
+
+        int t_ret = 0;
+        queue<Point> q_2;
+
         while (!q.empty())
         {
-            Point point = q.front();
+            Point qq = q.front();
             q.pop();
-
-            if (!visited[point.y][point.x])
+            if (!visited[qq.y][qq.x])
             {
-                visited[point.y][point.x] = true;
-                q_2.push({point.y, point.x});
-                cnt++;
+                visited[qq.y][qq.x] = true;
+                t_ret++;
+                q_2.push({qq.y, qq.x});
+
                 while (!q_2.empty())
                 {
                     Point search = q_2.front();
                     q_2.pop();
-                    for (int i = 0; i < 4; i++)
+                    for (int e = 0; e < 4; e++)
                     {
-                        int n_y = search.y + d_y[i];
-                        int n_x = search.x + d_x[i];
+                        int n_y = search.y + d_y[e];
+                        int n_x = search.x + d_x[e];
 
                         if (n_y >= 0 && n_y < N && n_x >= 0 && n_x < N)
                         {
-                            if (!visited[n_y][n_x] && arr[n_y][n_x] > height - 1)
+                            if (!visited[n_y][n_x] && arr[n_y][n_x] > height)
                             {
                                 visited[n_y][n_x] = true;
                                 q_2.push({n_y, n_x});
@@ -83,14 +81,14 @@ int main()
                 }
             }
         }
-        if (cnt > answer)
-        {
-            answer = cnt;
+
+        if(ret < t_ret){
+            ret = t_ret;
         }
         height--;
     }
 
-    cout << answer;
+    cout << ret;
 
     return 0;
 }
