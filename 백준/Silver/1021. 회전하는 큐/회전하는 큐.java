4ba926958f2ee9deca87f8b6c46372c338707368
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-01-29 20:56:47 +0900

    [Silver III] Title: 회전하는 큐, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,74 +1,67 @@
-import java.util.Scanner;
-import java.util.LinkedList;
- 
+
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		LinkedList<Integer> deque = new LinkedList<Integer>();
-		
-		int count = 0;	// 2, 3번 연산 횟수 누적 합 변수
-		
-		int N = in.nextInt();	// 큐의 크기(1 ~ N)
-		int M = in.nextInt();	// 뽑으려는 숫자의 개수
-		
-		// 1부터 N까지 덱에 담아둔다.
-		for(int i = 1; i <= N; i++) {
-			deque.offer(i);
-		}
-		
-		int[] seq = new int[M];	// 뽑고자 하는 수를 담은 배열
-		
-		for(int i = 0; i < M; i++) {
-			seq[i] = in.nextInt();
-		}
-		
-		
-		for(int i = 0; i < M; i++) {			
-			
-			// 덱에서 뽑고자 하는 숫자의 위치(index) 찾기 
-			int target_idx = deque.indexOf(seq[i]);
-			int half_idx;
-			/*
-			 *  만약 현재 덱의 원소가 짝수 개라면 중간 지점을 
-			 *  현재 덱의 절반 크기에서 -1 감소시킨다. 
-			 *  
-			 *  {1, 2, 3, 4} 일 때, 2를 중간지점으로 하면
-			 *  인덱스는 1이기 때문
-			 */
-			if(deque.size() % 2 == 0) {
-				half_idx = deque.size() / 2 - 1;
-			}
-			else {
-				half_idx = deque.size() / 2;
-			}
-			
-			
-			// 중간 지점 또는 중간 지점보다 원소의 위치가 앞에 있을 경우
-			if(target_idx <= half_idx) {
-				// idx 보다 앞에 있는 원소들을 모두 뒤로 보낸다. (2번 연산)
-				for(int j = 0; j < target_idx; j++) {
-					int temp = deque.pollFirst();
-					deque.offerLast(temp);
-					count++;
-				}
-			}
-			else {	// 중간 지점보다 원소의 위치가 뒤에 있는 경우 
-				// idx를 포함한 뒤에 있는 원소들을 모두 앞으로 보낸다. (3번 연산)
-				for(int j = 0; j < deque.size() - target_idx; j++) {
-					int temp = deque.pollLast();
-					deque.offerFirst(temp);
-					count++;
-				}
-			
-			}
-			deque.pollFirst();	// 연산이 끝나면 맨 앞 원소를 삭제
-		}
-		
-		System.out.println(count);
-		
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        Deque<Integer> deque = new ArrayDeque<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int d_value = 1;
+        // 데크 초기화
+        for (int i = 0; i < N; i++) {
+            deque.addLast(d_value++);
+        }
+        int[] arr = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            // N = 문서 갯수 / M = 언제 출력 되는지 알아내고 싶은 문서의 위치
+            int target_num = Integer.parseInt(st.nextToken());
+            arr[i] = target_num;
+        }
+        int total_rot = 0;
+        int target_idx = 0;
+
+        while (target_idx < arr.length) {
+            int cur = deque.peekFirst();
+
+            if (cur == arr[target_idx]) {
+                deque.removeFirst();
+                target_idx++;
+                if (target_idx == arr.length) {
+                    System.out.println(total_rot);
+                    break;
+                }
+            } else {
+                // 1. 목표 위치 찾기
+                int target_pos = 0;
+                for (int i : deque) {
+
+                    if (i == arr[target_idx]) {
+                        break;
+                    }
+                    target_pos++;
+                }
+
+                // 2. 좌우 회전 비교
+                int left_rot = target_pos;
+                int right_rot = deque.size() - target_pos;
+
+                // 3. 최소 회전 실행
+                if (left_rot <= right_rot) {
+                    deque.addLast(deque.removeFirst());  // 왼쪽 회전
+                } else {
+                    deque.addFirst(deque.removeLast());  // 오른쪽 회전
+                }
+                total_rot++;  // 무조건 +1
+            }
+
+        }
+
+
+        br.close();
+    }
 }
