352aea94dbe6fa20edc2c8a83be501c8467b1c10
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-01-31 11:17:51 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 28920 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,29 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for (int i = 1; i < N + 1; i++) {
+            dq.addLast(i);
+        }
+
+        while (dq.size() > 1) {
+            dq.removeFirst();
+            dq.addLast(dq.removeFirst());
+        }
+
+        System.out.println(dq.removeFirst());
+
+
+        br.close();
+    }
+}
