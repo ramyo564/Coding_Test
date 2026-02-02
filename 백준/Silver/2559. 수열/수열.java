596e9e8ed8a453eca622e82e4e89d25e2b7312b0
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-02-02 14:50:15 +0900

    [Silver III] Title: 수열, Time: 212 ms, Memory: 23788 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,35 +1,51 @@
-import java.io.*;
-import java.util.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 2559 : 수들의 합
+ * https://www.acmicpc.net/problem/2559
+ */
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+
+        int ans = 0, temp = 0, left = 0, right = K;
+
         int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
 
-        int ret = 0, temp = 0;
-        
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st2.nextToken());
-            arr[i] = num;
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < K; i++) {
-            ret += arr[i];
-        }
-        temp = ret;
-        int j = 0;
-        for ( int i = K; i < N; i++){
-            temp = temp - arr[j] + arr[i];
-            if (ret < temp){
-                ret = temp;
+        if (K == 1) {
+            Arrays.sort(arr);
+            System.out.println(arr[N - 1]);
+        } else {
+            for (int i = 0; i < K; i++) {
+                temp += arr[i];
             }
-            j++;
+
+            ans = temp;
+            while (right < N) {
+                temp = temp - arr[left++] + arr[right++];
+                ans = Math.max(ans, temp);
+            }
+
+            System.out.println(ans);
+
         }
-        System.out.println(ret);
+
+
     }
 }
