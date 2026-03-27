6c07d41785a4b12f788936a5d9270af3067172b6
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-03-27 18:11:14 +0900

    [level 2] Title: 할인 행사, Time: 84.53 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,24 +2,21 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-
         HashMap<String, Integer> wantMap = new HashMap<>();
         for (int i = 0; i < want.length; i++) {
             wantMap.put(want[i], number[i]);
         }
-
-        for (int i = 0; i <= discount.length - 10; i++) {
-            HashMap<String, Integer> tenDaysMap = new HashMap<>();
-
-            for (int j = i; j < i + 10; j++) {
-                tenDaysMap.put(discount[j], tenDaysMap.getOrDefault(discount[j], 0) + 1);
+        for (int i = 0; i < discount.length -9; i++) {
+            HashMap<String, Integer> temp = new HashMap<>();
+            int idx = i;
+            for (int j = 0; j < 10; j++) {
+                temp.put(discount[idx], temp.getOrDefault(discount[idx], 0) +1);
+                idx++;
             }
-            
-            if (wantMap.equals(tenDaysMap)) {
+            if(wantMap.equals(temp)){
                 answer++;
             }
         }
-
         return answer;
     }
 }
