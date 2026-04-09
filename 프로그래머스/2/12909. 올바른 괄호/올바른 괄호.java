b13b2ce704e312d2c05b0dd8922d2365eb862f21
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-04-09 10:09:49 +0900

    [level 2] Title: 올바른 괄호, Time: 6.03 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,17 +1,18 @@
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-
         int cnt = 0;
+
         for(char c : s.toCharArray()){
             if(c == '('){
-                cnt ++;
-            }else {
-                cnt --;
+                cnt++;
+            } else {
+                cnt--;
             }
             if(cnt < 0){
                 return false;
             }
+            
         }
 
         return cnt == 0;
