629a643bb09bfea586826024c6e83400c460a276
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-04-09 22:06:35 +0900

    [level 2] Title: 기능개발, Time: 20.18 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,28 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        
+        // 각 프로세스가 며칠 걸리는지 환산
         Queue<Integer> q = new LinkedList<>();
         for(int i = 0; i < speeds.length; i++){
-   int dayLeft = (int) Math.ceil((double) (100 - progresses[i]) / speeds[i]);
-            q.add(dayLeft);
-            
+            int remain = 100 - progresses[i];
+            int day = (int)Math.ceil((double)remain / speeds[i]);
+            q.add(day);
         }
-        
         List<Integer> list = new ArrayList<>();
-        int cnt = 0;
         while(!q.isEmpty()){
-            int target = q.peek();
-            q.poll();
-            cnt ++;
-            while (!q.isEmpty() && target >= q.peek()){
+            int now = q.peek();
+            int cnt = 0;
+            while(!q.isEmpty() && now >= q.peek()){
+                cnt++;
                 q.poll();
-                cnt++; 
             }
             list.add(cnt);
-            cnt = 0;
+            
         }
-        
         
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
