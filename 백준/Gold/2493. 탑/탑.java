0bff11b7ded35af12e89eb4c6c90dd47f8cae0a9
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-04-11 17:37:33 +0900

    [Gold V] Title: 탑, Time: 616 ms, Memory: 86940 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,38 +1,36 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
   public static void main(String[] args) throws IOException {
+
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
     StringTokenizer st = new StringTokenizer(br.readLine());
+
     int[] arr = new int[N];
-    Stack<Integer> stack = new Stack<>();
     for (int i = 0; i < N; i++) {
-      int num = Integer.parseInt(st.nextToken());
-      arr[i] = num;
+      int n = Integer.parseInt(st.nextToken());
+      arr[i] = n;
     }
 
-    int[] answer = new int[N];
-
+    int[] result = new int[N];
+    Deque<Integer> dq = new ArrayDeque<>();
     for (int i = N - 1; i >= 0; i--) {
-      while (!stack.isEmpty() && arr[i] >= arr[stack.peek()]) {
-        int idx = stack.pop();
-        answer[idx] = i + 1;
+      while (!dq.isEmpty() && arr[i] >= arr[dq.peek()]) {
+        int n = dq.pop();
+        result[n] = i + 1;
       }
-      stack.push(i);
-    }
-    // 큰 수가 없을 경우 0 처리
-    while (!stack.isEmpty()) {
-      int idx = stack.pop();
-      answer[idx] = 0;
-    }
-    // 출력 성능을 위해 StringBuilder 사용
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < N; i++) {
-      sb.append(answer[i]).append(' ');
+      dq.push(i);
     }
 
+    while (!dq.isEmpty()) {
+      result[dq.pop()] = 0;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (int num : result) {
+      sb.append(num).append(' ');
+    }
     System.out.println(sb.toString().trim());
     br.close();
   }
