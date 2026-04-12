4c8bd678f76b393cf949b406703fde7f7a79d175
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-04-12 14:35:52 +0900

    [Gold V] Title: 탑, Time: 632 ms, Memory: 87048 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,24 +1,23 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
   public static void main(String[] args) throws IOException {
-
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
     StringTokenizer st = new StringTokenizer(br.readLine());
-
     int[] arr = new int[N];
+
     for (int i = 0; i < N; i++) {
-      int n = Integer.parseInt(st.nextToken());
-      arr[i] = n;
+      arr[i] = Integer.parseInt(st.nextToken());
     }
 
-    int[] result = new int[N];
     Deque<Integer> dq = new ArrayDeque<>();
+    int[] result = new int[N];
     for (int i = N - 1; i >= 0; i--) {
-      while (!dq.isEmpty() && arr[i] >= arr[dq.peek()]) {
-        int n = dq.pop();
-        result[n] = i + 1;
+      while (!dq.isEmpty() && arr[dq.peek()] <= arr[i]) {
+        int idx = dq.pop();
+        result[idx] = i + 1;
       }
       dq.push(i);
     }
@@ -28,10 +27,11 @@ public class Main {
     }
 
     StringBuilder sb = new StringBuilder();
-    for (int num : result) {
-      sb.append(num).append(' ');
+    for (int i : result) {
+      sb.append(i).append(' ');
     }
+
     System.out.println(sb.toString().trim());
-    br.close();
+      
   }
 }
