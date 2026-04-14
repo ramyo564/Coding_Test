7f7b40ce1379d1d09db27ef8ab6914d72728591f
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-04-14 15:12:40 +0900

    [level 2] Title: 더 맵게, Time: 1561.80 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,21 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
+        int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+            
         for(int i : scoville){
-            pq.add(i);
-        }
-        int cnt = 0;
-        while(pq.peek() < K){
-            if(pq.size() == 1){
-                return -1;
-            }
-            int first = pq.poll();    
-            int second = pq.poll();
-            pq.add(first + (second * 2));
-            cnt++;
+            pq.offer(i);
         }
         
-        return cnt;
+        int cnt = 0;
+        while(pq.size() > 1){
+            if(pq.peek() < K){
+                int first = pq.poll();
+                int second = pq.poll();
+                pq.offer(first + (second * 2));
+                cnt++;
+            }else {
+                break;
+            }
+            
+        }
+        
+        if(pq.peek() >= K){
+            return cnt;
+        }else {
+            return -1;
+        }
     }
 }
