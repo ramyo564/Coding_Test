b658494305afd726c869d7e93625d2514773762d
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-04-14 14:22:35 +0900

    [Gold IV] Title: 오큰수, Time: 760 ms, Memory: 160036 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -3,45 +3,33 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
+
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
-
     StringTokenizer st = new StringTokenizer(br.readLine());
+
     int[] arr = new int[N];
     for (int i = 0; i < N; i++) {
-      arr[i] = Integer.parseInt(st.nextToken());
-    }
-    // 오른쪽 수가 현재보다 크면 넣는다
-    // 없으면 -1
-    // 현재 idx 보다 오른쪽이 크면서 가장 왼쪽에 있는 수를 넣는다
-
-    // 바로 이전꺼 확인하기
-    Stack<Integer> stack = new Stack<>();
-    // 정답 arr
-    int[] answer = new int[N];
-
-    for (int i = 0; i < arr.length; i++) {
-      while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
-        int idx = stack.peek();
-          answer[idx] = arr[i];
-          stack.pop();
-        
-      }
-      stack.push(i);
+      int num = Integer.parseInt(st.nextToken());
+      arr[i] = num;
     }
 
-    // 큰 수가 없을 경우 -1 처리
-    while (!stack.isEmpty()) {
-      int idx = stack.pop();
-      answer[idx] = -1;
-    }
-    // 출력 성능을 위해 StringBuilder 사용
-    StringBuilder sb = new StringBuilder();
+    Deque<Integer> dq = new ArrayDeque<>();
+    int[] ans = new int[N];
     for (int i = 0; i < N; i++) {
-      sb.append(answer[i]).append(' ');
+      while (!dq.isEmpty() && arr[dq.peek()] < arr[i]) {
+        int idx = dq.pop();
+        ans[idx] = arr[i];
+      }
+      dq.push(i);
     }
-
+    while (!dq.isEmpty()) {
+      ans[dq.pop()] = -1;
+    }
+    StringBuilder sb = new StringBuilder();
+    for (int i : ans) {
+      sb.append(i).append(' ');
+    }
     System.out.println(sb.toString().trim());
-    br.close();
   }
 }
