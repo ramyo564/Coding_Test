5aa453e3316feacb54c542ea60f77545463a903e
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-04-23 12:21:45 +0900

    [level 3] Title: 이중우선순위큐, Time: 100.51 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,33 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
+        
         TreeSet<Integer> ts = new TreeSet<>();
         int[] answer = new int[2];
         
-        for(String s : operations){
-            String[] temp = s.split(" ");
-            int n = Integer.parseInt(temp[1]);
+        for(String ss : operations){
+            String[] s = ss.split(" ");
+            int num = Integer.parseInt(s[1]);
             
-            if(s.startsWith("I")){
-                ts.add(n);
-            }else if (s.equals("D -1")){
-                if(!ts.isEmpty()){
-                    ts.pollFirst();
-                }
-            }else if (s.equals("D 1")){
-                if(!ts.isEmpty()){
-                    ts.pollLast();
-                }
+            if(ss.startsWith("I")){
+                ts.add(num);
+            }else if (ss.equals("D -1")){
+                ts.pollFirst();
+            }else if (ss.equals("D 1")){
+                ts.pollLast();
             }
         }
         if(ts.isEmpty()){
             return answer;
-        }else{
-            
+        }else {
             answer[0] = ts.last();
             answer[1] = ts.first();
         }
-        
         return answer;
     }
 }
