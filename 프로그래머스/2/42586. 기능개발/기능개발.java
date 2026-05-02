3c11a1da45809a21e40f828b9f13d404e966e6f3
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-02 19:32:00 +0900

    [level 2] Title: 기능개발, Time: 3.34 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        // 남은 작업일 계산
-        // deque에 넣음
-        // 처음 뽑은거 >= 다음꺼 = cnt ++
         int N = speeds.length;
         Deque<Integer> dq = new ArrayDeque<>();
-        for (int i = 0; i < N; i++){
-            int remain = 100;
-            dq.addLast((int) Math.ceil((double) (remain - progresses[i]) / speeds[i]));
-        }
-        
         List<Integer> list = new ArrayList<>();
-        int cnt = 0;
+        
+        for(int i = 0; i < N ; i++){
+            int f = progresses[i];
+            int s = speeds[i];
+            int remain = (int)Math.ceil((100-f) / (double)s);
+            // System.out.println(remain);    
+            dq.offer(remain);
+        }
+        // System.out.println(dq);
         while(!dq.isEmpty()){
-            int n = dq.pollFirst();
-            cnt++;
-            while(!dq.isEmpty() && n >= dq.peekFirst()){
-                dq.pollFirst();
+            int temp = dq.poll();
+            int cnt = 1;
+            while(!dq.isEmpty() && dq.peek() <= temp){
+                dq.poll();
                 cnt++;
             }
             list.add(cnt);
-            cnt = 0;
+            
         }
-        
-        return list.stream().mapToInt(Integer::intValue).toArray();
+
+        return list.stream().mapToInt(i->i).toArray();
     }
 }
