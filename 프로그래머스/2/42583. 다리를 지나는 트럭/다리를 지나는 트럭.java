4e1f02dd35f1828f647af3a044a386625efdecc8
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-03 13:23:59 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 19.36 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,34 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-            // 다리 모델링
-    Deque<Integer> bg = new ArrayDeque<>();
-    for (int i = 0; i < bridge_length; i++) {
-      bg.push(0);
-    }
-
-    // 전역 상태 값
-    int time = 0;
-    int cur_wt = 0;
-    // 트럭 인덱스
-    int idx = 0;
-
-    // 다리는 무게만 체크
-    while (true) {
-      cur_wt -= bg.poll();
-      time++;
-      if (idx < truck_weights.length && weight >= cur_wt + truck_weights[idx]) {
-        bg.offer(truck_weights[idx]);
-        cur_wt += truck_weights[idx];
-        idx++;
-      } else {
-        bg.offer(0);
-      }
-
-      if (cur_wt == 0) {
-        return time;
-      }
-    } 
         
+        int N = bridge_length;
+        int M = truck_weights.length;
+        Deque<Integer> bridgeDq = new ArrayDeque<>();
+        // 다리 초기화
+        for(int i = 0; i < N; i ++){
+            bridgeDq.offer(0);
+        }
+        
+        int idx = 0;
+        int curW = 0;
+        int time = 0;
+        while(true){
+            // 트럭 진입 가능
+            curW -= bridgeDq.poll();
+            if(idx < M && curW + truck_weights[idx] <= weight){
+                bridgeDq.offer(truck_weights[idx]);
+                curW += truck_weights[idx];
+                idx++;
+            }
+            // 트럭 진입 불가
+            else {
+                bridgeDq.offer(0);
+            }
+            time++;
+            if(idx == M && curW == 0){
+                break;
+            }
+
+        }
+        
+
+        return time;
     }
 }
