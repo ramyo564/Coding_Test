216a2440d087f43e304bd2109883d154f76e48cb
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-04 09:00:41 +0900

    [level 2] Title: 주식가격, Time: 28.62 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,23 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
-        Deque<Integer> dq = new ArrayDeque<>();
-        int[] ans = new int[prices.length];
-        for (int i = 0; i < prices.length; i++) {
-          // 1 2 3 2 3
-          // 0 1 2 3 4
-          while (!dq.isEmpty() && prices[dq.peek()] > prices[i]) {
-            int idx = dq.pop();
-            ans[idx] = i - idx;
-          }
-          dq.push(i);
-        }
-
-        while (!dq.isEmpty()) {
-          int idx = dq.pop();
-          ans[idx] = prices.length - idx - 1;
-        }
-        return ans;
+        int[] answer = {};
+        return answer;
     }
 }
