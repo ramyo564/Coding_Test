6f7e6d0f6c2b843803801cbd61153c1cec0adca5
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-06 17:49:05 +0900

    [level 3] Title: 베스트앨범, Time: 5.84 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,50 +1,38 @@
 import java.util.*;
+
 class Solution {
-    class Song {
-        int idx;
-        int play;
-        Song(int idx, int play){
-            this.idx = idx;
-            this.play = play;
-        }
-    }
     public int[] solution(String[] genres, int[] plays) {
-        int N = genres.length;
-        // 장르별 랭킹 해시맵
-        HashMap<String, Integer> rankG = new HashMap<>();
-        // 장르별 플래이 및 인덱스 처리
-        HashMap<String, ArrayList<Song>> gData= new HashMap<>();
-        for(int i = 0; i < N; i++){
+        class Song{
+            int idx;
+            int play;
+            Song(int idx, int play){
+                this.idx = idx;
+                this.play = play;
+            }
+        }
+        HashMap<String, Integer> rk = new HashMap<>();
+        HashMap<String, ArrayList<Song>> gen = new HashMap<>();
+        int N = plays.length;
+        
+        for (int i = 0; i < N; i ++){
             String genre = genres[i];
             int play = plays[i];
-            rankG.put(genre, rankG.getOrDefault(genre, 0) + play);
-            gData.putIfAbsent(genre, new ArrayList<Song>());
-            gData.get(genre).add(new Song(i, play));
+            rk.put(genre, rk.getOrDefault(genre, 0) + play);
+            gen.putIfAbsent(genre, new ArrayList<Song>());
+            gen.get(genre).add(new Song(i, play));
         }
         
-        // 장르별 랭킹 정렬
-        List<String> ranking = new ArrayList<>(rankG.keySet());
-        ranking.sort((a,b) -> rankG.get(b) - rankG.get(a));
+        List<String> gRk = new ArrayList<>(rk.keySet());
+        gRk.sort((a,b) -> rk.get(b) - rk.get(a));
         
-        // 정답 자료
         List<Integer> ans = new ArrayList<>();
-        
-        for(String g : ranking){
-            List<Song> data = gData.get(g);
-            data.sort((a,b) -> {
-                if(a.play == b.play){
-                    return 0;
-                }
-                return b.play - a.play;
-            });
-            
-            // 2개씩 뽑기
-            for(int i = 0; i < Math.min(data.size(), 2); i++ ){
-                ans.add(data.get(i).idx);
-            }    
+        for(String name : gRk){
+            List<Song> l = gen.get(name);
+            l.sort((a, b) -> b.play - a.play);
+            for(int i = 0; i < Math.min(2, l.size()); i++){
+                ans.add(l.get(i).idx);
+            }
         }
-        
-        
         
         return ans.stream().mapToInt(i -> i).toArray();
     }
