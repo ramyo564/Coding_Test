092642299cb9a1a2b747535190a4df7641b771b7
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-07 18:51:40 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.56 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,33 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+
         int N = jobs.length;
-        
-        Arrays.sort(jobs, (a,b) -> a[0]-b[0]);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        
-        // 작업 순서
-        int completedJobs = 0;
-        // 작업
-        int jobIdx = 0;
-        // 현재 시간
-        int curT = 0;
-        // ans
+        int curTime = 0;
+        int jIdx = 0;
+        int doneJob = 0;
         int ans = 0;
-        
-        while (completedJobs < N) {
-          // 현재시간 >= 요청시간
-          while (jobIdx < N && curT >= jobs[jobIdx][0]) {
-            pq.offer(jobs[jobIdx++]);
+        while (doneJob < N) {
+          while (jIdx < N && curTime >= jobs[jIdx][0]) {
+            pq.offer(jobs[jIdx++]);
           }
           if (pq.isEmpty()) {
-              curT = jobs[jobIdx][0];
+            curTime = jobs[jIdx][0];
           } else {
             int[] data = pq.poll();
-            ans += (curT - data[0]) + data[1];
-            curT += data[1];
-            completedJobs++;
+            ans += curTime - data[0] + data[1];
+            curTime += data[1];
+            doneJob++;
           }
         }
 
