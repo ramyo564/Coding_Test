4b236d5504bda158a89005b51d17e386113874d9
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-10 12:46:08 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.57 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,6 +1,47 @@
+import java.util.*;
 class Solution {
+    static ArrayList<Integer>[] list;
     public int solution(int n, int[][] wires) {
-        int answer = -1;
-        return answer;
+        list = new ArrayList[n+1];
+        
+        for(int i = 1; i <= n; i++){
+            list[i] = new ArrayList<>();
+        }
+        
+        for(int[] wire : wires){
+            list[wire[0]].add(wire[1]);
+            list[wire[1]].add(wire[0]);
+        }
+        
+        int midN = n;
+        for(int[] wire : wires){
+            int cnt1 = bfs(wire, n);
+            int cnt2 = n - cnt1;
+            midN = Math.min(midN, Math.abs(cnt1-cnt2));
+        }
+    
+        
+        return midN;
+    }
+    private int bfs(int[]wire , int n){
+        boolean[] visited = new boolean[n+1];
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        int start = wire[0];
+        int skip = wire[1];
+        dq.offer(start);
+        visited[start] = true;
+        int cnt = 1;
+        while(!dq.isEmpty()){
+            int curr = dq.poll();
+            for(int next : list[curr]){
+                if(next != skip && !visited[next]){
+                    visited[next] = true;
+                    cnt++;
+                    dq.offer(next);
+                }
+            }
+        }
+        return cnt;
     }
 }
