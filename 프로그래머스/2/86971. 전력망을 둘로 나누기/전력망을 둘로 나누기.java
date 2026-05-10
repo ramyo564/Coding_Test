995209a002f641324c867c55a1cad58425802bbb
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-10 12:01:10 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.33 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,51 +1,45 @@
 import java.util.*;
 
 class Solution {
-  List<Integer>[] list;
-
-  public int solution(int n, int[][] wires) {
-    // 전력망 초기화
-    list = new ArrayList[n + 1];
-    for (int i = 1; i <= n; i++) {
-      list[i] = new ArrayList<>();
-    }
-    // 전력망 배치하기
-    for (int[] wire : wires) {
-      int first = wire[0];
-      int second = wire[1];
-      list[first].add(second);
-      list[second].add(first);
-    }
-
-    System.out.println(Arrays.toString(list));
-    int minNum = n;
-    for (int[] wire : wires) {
-      int cnt1 = bfs(wire, n);
-      int cnt2 = n - cnt1;
-      minNum = Math.min(minNum, Math.abs(cnt1 - cnt2));
-    }
-    return minNum;
-  }
-
-  private int bfs(int[] wire, int n) {
-    boolean[] visited = new boolean[n + 1];
-    Deque<Integer> dq = new ArrayDeque<>();
-    int start = wire[0];
-    int skip = wire[1];
-    int cnt = 1;
-    visited[start] = true;
-    dq.offer(start);
-
-    while (!dq.isEmpty()) {
-      int curr = dq.poll();
-      for (int next : list[curr]) {
-        if (next != skip && !visited[next]) {
-          visited[next] = true;
-          dq.offer(next);
-          cnt++;
+    static List<Integer>[] list;
+    public int solution(int n, int[][] wires) {
+        list = new ArrayList[n+1];
+        for(int i = 1; i <= n ; i ++){
+            list[i] = new ArrayList<>();
         }
-      }
+        
+        for(int[] wire : wires){
+            list[wire[0]].add(wire[1]);
+            list[wire[1]].add(wire[0]);
+        }
+        int midNum = n;
+        for(int[] wire : wires){
+            int cnt1 = bfs(wire, n);
+            int cnt2 = n - cnt1;
+            midNum = Math.min(midNum, Math.abs(cnt1-cnt2));
+        }
+        
+        
+        return midNum;
     }
-    return cnt;
-  }
+    static int bfs(int[] wire, int n){
+        boolean[] visited = new boolean[n+1];
+        Deque<Integer> dq = new ArrayDeque<>();
+        int start = wire[0];
+        int skip = wire[1];
+        dq.add(start);
+        visited[start] = true;
+        int cnt = 1;
+        while(!dq.isEmpty()){
+            int curr = dq.poll();
+            for(int i : list[curr]){
+                if(i != skip && !visited[i]){
+                    visited[i] = true;
+                    cnt++;
+                    dq.add(i);
+                }
+            }
+        }
+        return cnt;
+    }
 }
