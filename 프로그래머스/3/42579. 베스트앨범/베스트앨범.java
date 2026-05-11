2c4e9823b033604198a18c7721eaf8bea70aedfd
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-11 21:55:11 +0900

    [level 3] Title: 베스트앨범, Time: 3.10 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -10,32 +10,28 @@ class Solution {
                 this.play = play;
             }
         }
-        int N = plays.length;
         HashMap<String, Integer> gRank = new HashMap<>();
-        HashMap<String, ArrayList<Song>> gens = new HashMap<>();
-        
-        for(int i = 0; i < N; i++){
+        HashMap<String, ArrayList<Song>> songs = new HashMap<>();
+        for(int i = 0; i < plays.length; i++){
             String g = genres[i];
-            int ply = plays[i];
-            gRank.put(g, gRank.getOrDefault(g, 0) + ply);
-            gens.putIfAbsent(g, new ArrayList<Song>());
-            gens.get(g).add(new Song(i, ply));
-            
+            int p = plays[i];
+            gRank.put(g, gRank.getOrDefault(g, 0) + p);
+            songs.putIfAbsent(g, new ArrayList<Song>());
+            songs.get(g).add(new Song(i, p));
         }
         
-        List<String> rk = new ArrayList<>(gRank.keySet());
-        rk.sort((a,b) -> gRank.get(b) - gRank.get(a));
+        ArrayList<String> list = new ArrayList<>(gRank.keySet());
+        list.sort((a,b)-> gRank.get(b) - gRank.get(a));
         
-        List<Integer> ans = new ArrayList<>();
-        for(String genreName : rk){
-            List<Song> info = new ArrayList<>(gens.get(genreName));
-            info.sort((a,b) -> b.play - a.play);
-            for(int i = 0; i < Math.min(2, info.size()); i++){
-                ans.add(info.get(i).idx);
+        List<Integer> res = new ArrayList<>();
+        for(String s : list){
+            ArrayList<Song> slist = songs.get(s);
+            slist.sort((a,b) -> b.play - a.play);
+            for(int i = 0; i < Math.min(slist.size(), 2); i++){
+                res.add(slist.get(i).idx);
             }
-            
         }
         
-        return ans.stream().mapToInt(i -> i).toArray();
+        return res.stream().mapToInt(i->i).toArray();
     }
 }
