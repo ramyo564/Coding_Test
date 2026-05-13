666c78486d85c75502e3983e9696d561f023d5e2
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-13 17:06:22 +0900

    [level 2] Title: 숫자 변환하기, Time: 39.02 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,37 +1,33 @@
 import java.util.*;
+
 class Solution {
     public int solution(int x, int y, int n) {
-        boolean[] visited = new boolean[y+1];
         Deque<int[]> dq = new ArrayDeque<>();
+        boolean[] visited = new boolean[y+1];
         dq.offer(new int[]{x , 0});
         visited[x] = true;
         
         while(!dq.isEmpty()){
             int[] data = dq.poll();
-            int X = data[0];
-            int V = data[1];
-            if(X == y){
-                return V;
+            int num = data[0];
+            int cnt = data[1];
+            if(num == y){
+                return cnt;
             }
-            int temp1 = X * 3;
-            int temp2 = X * 2;
-            int temp3 = X + n;
-            if(y >= temp1 && !visited[temp1]){
-                dq.offer(new int[]{temp1, V+1});
+            int temp1 = num * 2;
+            int temp2 = num * 3;
+            int temp3 = num + n;
+            if(temp1 < y+1 && !visited[temp1]){
+                dq.offer(new int[]{temp1, cnt + 1});
                 visited[temp1] = true;
+            }if(temp2 < y+1 && !visited[temp2]){
+                dq.offer(new int[]{temp2, cnt + 1});
+                visited[temp2] = true;    
+            }if(temp3 < y+1 && !visited[temp3]){
+                dq.offer(new int[]{temp3, cnt + 1});
+                visited[temp3] = true;    
             }
-            if(y >= temp2 && !visited[temp2]){
-                dq.offer(new int[]{temp2, V+1});
-                visited[temp2] = true;
-            }
-            if(y >= temp3 && !visited[temp3]){
-                dq.offer(new int[]{temp3, V+1});
-                visited[temp3] = true;
-            }
-            
-            
         }
-        
         int answer = -1;
         return answer;
     }
