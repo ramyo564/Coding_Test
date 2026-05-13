4664d89f674bb55740ee6d50f213e36a82f3f5c1
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-13 17:06:52 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,34 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int x, int y, int n) {
-        Deque<int[]> dq = new ArrayDeque<>();
-        boolean[] visited = new boolean[y+1];
-        dq.offer(new int[]{x , 0});
-        visited[x] = true;
-        
-        while(!dq.isEmpty()){
-            int[] data = dq.poll();
-            int num = data[0];
-            int cnt = data[1];
-            if(num == y){
-                return cnt;
-            }
-            int temp1 = num * 2;
-            int temp2 = num * 3;
-            int temp3 = num + n;
-            if(temp1 < y+1 && !visited[temp1]){
-                dq.offer(new int[]{temp1, cnt + 1});
-                visited[temp1] = true;
-            }if(temp2 < y+1 && !visited[temp2]){
-                dq.offer(new int[]{temp2, cnt + 1});
-                visited[temp2] = true;    
-            }if(temp3 < y+1 && !visited[temp3]){
-                dq.offer(new int[]{temp3, cnt + 1});
-                visited[temp3] = true;    
-            }
-        }
-        int answer = -1;
+        int answer = 0;
         return answer;
     }
 }
