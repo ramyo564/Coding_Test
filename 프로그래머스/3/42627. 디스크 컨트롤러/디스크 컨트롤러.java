0bda263a47b199674615dc63a110289d06c3bacd
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-14 19:34:26 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,33 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, (a,b) -> a[0] - b[0]);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1] - b[1]);
-        
-        int N = jobs.length;
-        int jIdx = 0;
-        int curT = 0;
-        int doneJ = 0;
-        int ansT = 0;
-        
-        while(doneJ < N){
-
-            while(jIdx < N && curT >= jobs[jIdx][0]){
-                pq.offer(new int[] {jobs[jIdx][0], jobs[jIdx][1]});
-                jIdx++;
-            }
-            if(pq.isEmpty()){
-                curT = jobs[jIdx][0];
-            }
-            else{
-                int[] data = pq.poll();
-                curT += data[1];
-                ansT += curT - data[0];
-                doneJ++;
-            }
-            
-        }
-
-        return ansT/N;
+        int answer = 0;
+        return answer;
     }
 }
