8b00d6030598c823a138ae93c66c9f2414735f8d
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-15 13:10:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.47 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,47 +1,44 @@
 import java.util.*;
+
 class Solution {
-    static ArrayList<Integer>[] list;
+    static List<Integer>[] list;
+    static boolean[] visited;
     public int solution(int n, int[][] wires) {
         list = new ArrayList[n+1];
-        
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i <= n; i++){
             list[i] = new ArrayList<>();
         }
-        
         for(int[] wire : wires){
             list[wire[0]].add(wire[1]);
             list[wire[1]].add(wire[0]);
         }
-        
-        int midN = n;
+        int min = n;
         for(int[] wire : wires){
             int cnt1 = bfs(wire, n);
             int cnt2 = n - cnt1;
-            midN = Math.min(midN, Math.abs(cnt1-cnt2));
+            min = Math.min(min, Math.abs(cnt1-cnt2));
         }
-    
         
-        return midN;
+        
+        return min;
     }
-    private int bfs(int[]wire , int n){
-        boolean[] visited = new boolean[n+1];
+    static int bfs(int[] wire, int n){
         Deque<Integer> dq = new ArrayDeque<>();
-
-        int start = wire[0];
+        visited = new boolean[n+1];
+        int curr = wire[0];
         int skip = wire[1];
-        dq.offer(start);
-        visited[start] = true;
-        int cnt = 1;
+        dq.offer(curr);
+        visited[curr] = true;
+        int res = 1;
         while(!dq.isEmpty()){
-            int curr = dq.poll();
-            for(int next : list[curr]){
-                if(next != skip && !visited[next]){
-                    visited[next] = true;
-                    cnt++;
+            for(int next : list[dq.poll()]){
+                if(next != skip && !visited[next]){    
                     dq.offer(next);
-                }
+                    visited[next]=true;
+                    res++;
+                }        
             }
         }
-        return cnt;
+        return res;
     }
 }
