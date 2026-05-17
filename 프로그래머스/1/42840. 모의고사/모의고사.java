bc6a5a3a1883d3902d90804a973a31b0d7118782
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-18 08:57:48 +0900

    [level 1] Title: 모의고사, Time: 1.86 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,39 +1,36 @@
 import java.util.*;
 class Solution {
+    // 5
+    static int[] A = new int[] {1, 2, 3, 4, 5,};
+    // 8
+    static int[] B = new int[] { 2, 1, 2, 3, 2, 4, 2, 5};
+    // 10
+    static int[] C = new int[] {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    
+    static int[] res = new int[3];
+    
     public int[] solution(int[] answers) {
-        int[] answer = {};
-        // 5
-        int[] A = new int[]{1, 2, 3, 4, 5};
-        // 8
-        int[] B = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
-        // 10
-        int[] C = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
-        int[] data = new int[3];
-        for(int i = 0; i < answers.length; i++){
-            if(answers[i] == A[i%5]){
-                data[0]++;
+        for(int i = 0; i < answers.length; i ++){
+            if(A[i%5] == answers[i]){
+                res[0]++;
             }
-            if(answers[i] == B[i%8]){
-                data[1]++;
+            if(B[i%8] == answers[i]){
+                res[1]++;
             }
-            if(answers[i] == C[i%10]){
-                data[2]++;
+            if(C[i%10] == answers[i]){
+                res[2]++;
             }
         }
-        
-        int maxPoint = 0;
-        for(int i : data){
-            maxPoint = Math.max(maxPoint, i);
+        int max = 0;
+        for(int num : res){
+            max = Math.max(num, max);
         }
         List<Integer> list = new ArrayList<>();
-        
-        for(int i = 0; i < data.length; i++){
-            if (data[i] == maxPoint){
+        for(int i = 0; i< res.length; i ++){
+            if(res[i] == max){
                 list.add(i+1);
             }
         }
-        
         
         return list.stream().mapToInt(i->i).toArray();
     }
