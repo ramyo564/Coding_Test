61b6a47ef31a1b75fd23a5ba91076fe74e37cb4e
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-19 18:27:47 +0900

    [level 2] Title: 소수 찾기, Time: 16.45 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,31 +1,32 @@
 import java.util.*;
+
 class Solution {
-    static Set<Integer> set = new HashSet<>();
+    Set<Integer>set = new HashSet<>();
     public int solution(String numbers) {
-        int cnt = 0; 
+        int answer = 0;
         recursive("", numbers);
         for(int i : set){
             if(isPrime(i)){
-                cnt++;
+                answer++;
             }
         }
-        return cnt;
+        return answer;
     }
-    static void recursive(String comb, String others){
+    private void recursive(String comb, String others){
         if(!comb.equals("")){
             set.add(Integer.parseInt(comb));
         }
-        for(int i = 0; i < others.length(); i++){
-            recursive(comb+others.charAt(i), others.substring(0, i) + others.substring(i+1));
+        for(int i = 0; i < others.length(); i ++){
+            recursive(comb+others.charAt(i), others.substring(0,i) + others.substring(i+1));
         }
     }
     
-    static boolean isPrime(int num){
-        if(num < 2){
+    private boolean isPrime(int n){
+        if(n < 2){
             return false;
         }
-        for(int i = 2; i<= (int)Math.sqrt(num); i++){
-            if(num % i == 0){
+        for(int i = 2; i <= (int)Math.sqrt(n); i++){
+            if(n % i == 0){
                 return false;
             }
         }
