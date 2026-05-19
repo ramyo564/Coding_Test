5cbe4e8a76a73fd0afb6dfd959dae708f00d58ee
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-19 18:16:15 +0900

    [level 2] Title: 모음 사전, Time: 2.80 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,19 +1,22 @@
 import java.util.*;
+
 class Solution {
-    static String[] words = new String[]{"A","E","I","O","U"};
+    static String[] words = {"A","E","I","O","U"};
     static List<String> list = new ArrayList<>();
-    
     public int solution(String word) {
-        recursive("");
-        return list.indexOf(word);
+        dfs("");
+        
+        return list.indexOf(word)+1;
     }
-    static void recursive(String word){
-        list.add(word);
-        if(word.length() == 5){
+    private void dfs(String data){
+        if(!data.equals("")){
+            list.add(data);
+        }
+        if(data.length() == 5){
             return;
         }
-        for(int i = 0; i < 5; i ++){
-            recursive(word + words[i]);
+        for(int i = 0; i < words.length; i++){
+            dfs(data+words[i]);
         }
     }
 }
