3b3ca00e1c5266f72a376d23c3542f9158d90d5f
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-20 15:23:10 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,15 +2,20 @@ import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int total = brown + yellow;
-        
-        for (int h = 3; h <= total; h++) { // 세로 길이는 최소 3
-            if (total % h == 0) {
-                int w = total / h;
-                if ((w - 2) * (h - 2) == yellow) {
-                    return new int[]{w, h};
+    
+        while(true){
+            for(int i = 3; i < total; i++){
+                if(total % i == 0){
+                    int y = i;
+                    int x = total/i;
+                    if((y-2) * (x-2) == yellow){
+                        return new int[]{x,y};
+                    }
                 }
             }
+            break;
         }
-        return new int[]{};
+
+        return null;
     }
 }
