794f3b598d3387af3f59708425af156cf4e349c7
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-21 19:38:53 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.55 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,40 +3,41 @@ import java.util.*;
 class Solution {
     static List<Integer>[] list;
     static boolean[] visited;
+    static int V;
     public int solution(int n, int[][] wires) {
-        list = new ArrayList[n+1];
-        for(int i = 0; i <= n; i++){
-            list[i] = new ArrayList<>();
-        }
+        list = new ArrayList[n + 1];
+
+        V = n;
+        for(int i = 0; i < n+1; i ++) list[i] = new ArrayList<>();    
+        
         for(int[] wire : wires){
             list[wire[0]].add(wire[1]);
             list[wire[1]].add(wire[0]);
         }
-        int min = n;
+        
         for(int[] wire : wires){
-            int cnt1 = bfs(wire, n);
+            int cnt1 = func(wire, n);
             int cnt2 = n - cnt1;
-            min = Math.min(min, Math.abs(cnt1-cnt2));
+            V = Math.min(V, Math.abs(cnt1 - cnt2));
         }
-        
-        
-        return min;
+        return V;
     }
-    static int bfs(int[] wire, int n){
+    private int func(int[] wire, int n){
         Deque<Integer> dq = new ArrayDeque<>();
         visited = new boolean[n+1];
-        int curr = wire[0];
+        int start = wire[0];
         int skip = wire[1];
-        dq.offer(curr);
-        visited[curr] = true;
+        visited[start] = true;
+        dq.offer(start);
         int res = 1;
+        
         while(!dq.isEmpty()){
             for(int next : list[dq.poll()]){
-                if(next != skip && !visited[next]){    
+                if(next != skip && !visited[next]){
+                    visited[next] = true;
                     dq.offer(next);
-                    visited[next]=true;
                     res++;
-                }        
+                }
             }
         }
         return res;
