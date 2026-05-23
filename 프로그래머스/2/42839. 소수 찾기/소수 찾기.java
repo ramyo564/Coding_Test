19092083998f8f528a2cbf4927ee93e80001478a
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-23 16:24:31 +0900

    [level 2] Title: 소수 찾기, Time: 17.96 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,35 +1,37 @@
 import java.util.*;
 
 class Solution {
-    Set<Integer>set = new HashSet<>();
+    static Set<Integer> set = new HashSet<>();
     public int solution(String numbers) {
-        int answer = 0;
         recursive("", numbers);
+        int cnt = 0;
         for(int i : set){
             if(isPrime(i)){
-                answer++;
+                cnt++;
             }
         }
-        return answer;
+        return cnt;
     }
-    private void recursive(String comb, String others){
+    
+    static void recursive(String comb, String others){
         if(!comb.equals("")){
             set.add(Integer.parseInt(comb));
         }
-        for(int i = 0; i < others.length(); i ++){
+        for(int i = 0; i < others.length(); i++){
             recursive(comb+others.charAt(i), others.substring(0,i) + others.substring(i+1));
         }
     }
     
-    private boolean isPrime(int n){
+    static boolean isPrime(int n){
         if(n < 2){
             return false;
         }
-        for(int i = 2; i <= (int)Math.sqrt(n); i++){
+        for(int i = 2; i <= Math.sqrt(n); i++){
             if(n % i == 0){
                 return false;
             }
         }
+        
         return true;
     }
 }
