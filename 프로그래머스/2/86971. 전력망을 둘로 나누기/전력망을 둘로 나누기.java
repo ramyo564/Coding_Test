745d536d417d8dce890c0b3a02849fd317245ff8
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-23 12:11:53 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.85 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,43 +3,43 @@ import java.util.*;
 class Solution {
     static List<Integer>[] list;
     static boolean[] visited;
-    static int V;
     public int solution(int n, int[][] wires) {
-        list = new ArrayList[n + 1];
-
-        V = n;
-        for(int i = 0; i < n+1; i ++) list[i] = new ArrayList<>();    
-        
+        list = new ArrayList[n+1];
+        for (int i = 0; i <= n; i ++){
+            list[i] = new ArrayList<>();
+        }
         for(int[] wire : wires){
             list[wire[0]].add(wire[1]);
             list[wire[1]].add(wire[0]);
         }
-        
+        int res = n;
         for(int[] wire : wires){
-            int cnt1 = func(wire, n);
+            int cnt1 = bfs(wire, n);
             int cnt2 = n - cnt1;
-            V = Math.min(V, Math.abs(cnt1 - cnt2));
+            res = Math.min(res, Math.abs(cnt1-cnt2));
         }
-        return V;
+        
+        return res;
     }
-    private int func(int[] wire, int n){
+    private int bfs(int[] wire, int n){
         Deque<Integer> dq = new ArrayDeque<>();
         visited = new boolean[n+1];
         int start = wire[0];
         int skip = wire[1];
         visited[start] = true;
         dq.offer(start);
-        int res = 1;
-        
+        int cnt = 1;
         while(!dq.isEmpty()){
-            for(int next : list[dq.poll()]){
+            int curr = dq.poll();
+            for(int next : list[curr]){
                 if(next != skip && !visited[next]){
-                    visited[next] = true;
                     dq.offer(next);
-                    res++;
+                    visited[next] = true;
+                    cnt++;
                 }
             }
         }
-        return res;
+        
+        return cnt;
     }
 }
