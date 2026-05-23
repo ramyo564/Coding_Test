b6dc3a64f6c5e9c54a7894021468067d7ea2244e
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-23 17:35:51 +0900

    [level 2] Title: 숫자 변환하기, Time: 45.27 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,35 +1,32 @@
 import java.util.*;
 class Solution {
     public int solution(int x, int y, int n) {
-        
-        Deque<int[]> dq = new ArrayDeque<>();
         boolean[] visited = new boolean[y+1];
+        Deque<int[]> dq = new ArrayDeque<>();
         dq.offer(new int[]{x, 0});
-        visited[x] = true;
         while(!dq.isEmpty()){
             int[] data = dq.poll();
-            int num = data[0];
+            int curr = data[0];
             int cnt = data[1];
-            if(num == y){
+            if(curr == y){
                 return cnt;
             }
-            int temp1 = num * 2;
-            int temp2 = num * 3;
-            int temp3 = num + n;
+            int temp1 = curr*2;
+            int temp2 = curr*3;
+            int temp3 = curr+n;
             if(temp1 <= y && !visited[temp1]){
                 visited[temp1] = true;
-                dq.offer(new int[]{temp1, cnt + 1});
+                dq.offer(new int[]{temp1, cnt+1});
             }
             if(temp2 <= y && !visited[temp2]){
                 visited[temp2] = true;
-                dq.offer(new int[]{temp2, cnt + 1});
+                dq.offer(new int[]{temp2, cnt+1});
             }
             if(temp3 <= y && !visited[temp3]){
                 visited[temp3] = true;
-                dq.offer(new int[]{temp3, cnt + 1});
+                dq.offer(new int[]{temp3, cnt+1});
             }
         }
-        
         
         return -1;
     }
