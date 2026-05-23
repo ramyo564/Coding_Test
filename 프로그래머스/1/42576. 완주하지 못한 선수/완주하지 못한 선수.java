4713e39f3d5235ce264c7d7646215e253f5eff93
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-05-23 16:01:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.47 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,20 +1,20 @@
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
         HashMap<String, Integer> map = new HashMap<>();
+        
         for(String s : participant){
-            map.put(s, map.getOrDefault(s, 0) + 1);
+            map.put(s,map.getOrDefault(s, 0) +1);
         }
         for(String s : completion){
-            map.put(s, map.get(s) - 1);
+            map.put(s, map.get(s) -1);
         }
         for(Map.Entry<String, Integer> m : map.entrySet()){
-            if(m.getValue() > 0){
+            if(m.getValue() != 0){
                 return m.getKey();
             }
         }
-        String answer = "";
-        return answer;
+        
+        return null;
     }
 }
