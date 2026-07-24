98ff364b53ea5b0f2d3625cba38abc3332aa7f3d
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-07-24 15:58:35 +0900

    [level 2] Title: 피로도, Time: 2.68 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -5,14 +5,19 @@ class Solution {
     static int max = 0;
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
-        dfs(k, dungeons, 0);
+        dfs(dungeons, k, 0);
+        
         return max;
     }
-    private static void dfs(int k, int[][] dungeons, int cnt){
-        for(int i = 0; i < dungeons.length; i++){
-            if(!visited[i] && k >= dungeons[i][0]){
+    
+    static void dfs(int[][] dun, int k, int cnt){
+
+        for (int i = 0; i < dun.length; i++){
+            int need = dun[i][0];
+            int spend = dun[i][1];
+            if(!visited[i] && k >= need){
                 visited[i] = true;
-                dfs(k - dungeons[i][1], dungeons, cnt + 1);
+                dfs(dun, k-spend, cnt + 1);
                 visited[i] = false;
             }
         }
