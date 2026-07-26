3d607c22596eef98d2ddc5b0cc94e18f3007bab1
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-07-26 15:50:56 +0900

    [level 2] Title: 전화번호 목록, Time: 184.00 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,14 +1,14 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        int N = phone_book.length;
-        Arrays.sort(phone_book);
-        for(int i = 1; i < N; i++){
-            if(phone_book[i].startsWith(phone_book[i-1])){
+        Arrays.sort(phone_book, (a,b)-> a.compareTo(b));
+        
+        for(int i = 0; i < phone_book.length-1; i ++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
                 return false;
             }
         }
-        return answer;
+        
+        return true;
     }
 }
