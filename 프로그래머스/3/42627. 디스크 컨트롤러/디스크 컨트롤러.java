5ef188137ae029e5175868b21fbc7176951943f1
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2026-07-26 15:33:38 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.57 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,31 +1,30 @@
-import java.util.*;
+import java.util.*; 
+
 class Solution {
     public int solution(int[][] jobs) {
         Arrays.sort(jobs, (a,b)-> a[0] - b[0]);
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1] - b[1]);
-        
-        int N = jobs.length;
-        int doneJ = 0;
-        int idx = 0;
-        int curTime  = 0;
+        int len = jobs.length;
+        int jIdx = 0;
+        int done = 0;
+        int curTime = 0;
         int res = 0;
         
-        while(doneJ < N){
-            while(idx < N && curTime >= jobs[idx][0]){
-                pq.offer(jobs[idx]);
-                idx++;
+        while(done < len){
+            while(jIdx < len && curTime >= jobs[jIdx][0]){
+                pq.offer(jobs[jIdx++]);
             }
             if(pq.isEmpty()){
-                curTime = jobs[idx][0];
-            } else {
-                int[] data = pq.poll();
-                curTime += data[1];
-                res += curTime - data[0];
-                doneJ ++;
-                
+                curTime = jobs[jIdx][0];
             }
-            
+            else{
+                int[] now = pq.poll();
+                curTime += now[1];
+                res += curTime - now[0];
+                done++;
+            }
         }
-        return res / N;
+        
+        return res/len;
     }
 }
